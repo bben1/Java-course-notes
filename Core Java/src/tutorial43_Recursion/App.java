ca30bb339e82d4130195a023e5b5b1657d4fdfe9
brookerben1 <brooker.ben1@gmail.com>
2020-09-28 08:57:53 +0100

    Set collections tutorial

--- Core Java/src/tutorial43_Recursion/App.java
+++ Core Java/src/tutorial43_Recursion/App.java
@@ -2,20 +2,4 @@ package tutorial43_Recursion;
 
 public class App {
 
-	public static void main(String[] args) {
-		
-		calculateFactorial(5);
-
-	}
-
-	private static void calculateFactorial(int iint) {
-		
-		int factorial = 0;
-		
-		i = i-1
-				
-		
-		
-	}
-
 }
